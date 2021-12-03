5f209cf85057ab4ea4fd30a2f6a630e7070fb46a
Popkov Ilya <popkov.ilya.alexandrovich@gmail.com>
2021-12-03 14:00:18 +0300

    67

---
+++ [Хакинг. Искусство эксплойта]/intput.c
@@ -0,0 +1,14 @@
+#include <sdtio.h>
+#include <string.h>
+
+int main(){
+    char message[10];
+    int count, i;
+    strcpy(message, "Hello, world!\n");
+    printf("Сколько раз повторить");
+    scanf("%d", &count);
+
+    for(int i=0; i<count; i++){
+        printf("%3d - %s\n", i, message);
+    }
+}
