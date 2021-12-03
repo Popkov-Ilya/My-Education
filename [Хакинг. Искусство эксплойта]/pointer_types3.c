9c5a58b7ed2fc243a9f376da1e6e6a0afacd4897
Popkov Ilya <popkov.ilya.alexandrovich@gmail.com>
2021-12-03 17:21:04 +0300

    71

--- [Хакинг. Искусство эксплойта]/pointer_types3.c
+++ [Хакинг. Искусство эксплойта]/pointer_types3.c
@@ -7,17 +7,17 @@ int main(){
     int int_array[5] = {1, 2, 3, 4, 5};
     char *char_pointer;
     int *int_pointer;
-    char_pointer = int_array; //здесь все наоборот
-    int_pointer = char_array; //и здесь
+    char_pointer = (char *) int_array; // здесь мы объясняем char_pointer что он все равно остается собой
+    int_pointer = (int *) char_array;  // вне зависимости от того, на что он указывает
 
     for(i=0; i<5; i++){
         printf("[integer pointer] указывает на адрес %p, сожержащий целое число '%c'\n", int_pointer, *int_pointer);
-        int_pointer = int_pointer + 1; //не знаю почему не инкремент
+        int_pointer = (int *) ((char *) int_pointer + 1); // прибавляем 1, так как к char pointer прибавляется 1
     }
 
     for(i=0; i<5; i++){
         printf("[char_pointer] указывает на адрес %p, содержащий символ %d\n", char_pointer, *char_pointer);
-        char_pointer = char_pointer + 1;
+        char_pointer = (char *) ((int *) char_pointer + 1); // прибавляем 4, так как привели к int *
     }
 
 }
