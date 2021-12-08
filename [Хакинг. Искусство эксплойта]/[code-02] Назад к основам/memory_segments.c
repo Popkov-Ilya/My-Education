cfeecd79ed52f5612421ae348724419b8d1c72b9
Popkov Ilya <popkov.ilya.alexandrovich@gmail.com>
2021-12-08 16:41:50 +0300

    94

--- [Хакинг. Искусство эксплойта]/[code-02] Назад к основам/memory_segments.c
+++ [Хакинг. Искусство эксплойта]/[code-02] Назад к основам/memory_segments.c
@@ -5,23 +5,25 @@ int global_initialized_var = 5;
 
 void function(){
     int stack_var;
-    printf("stack_var (function) – \t0x%08x\n", &stack_var);
+    printf("0x%08x: stack_var (function)\n", &stack_var);
 }
 
 int main(){
-    int stack_var = 5;
+    int stack_var;
+    int stack_var2 = 5;
     static int static_initialized_var = 5;
     static int static_var;
     int *heap_var_ptr;
 
     heap_var_ptr = (int *)malloc(4); // malloc возвращает void*
 
-    printf("global_initialized_var - \t0x%08x\n", &global_initialized_var);
-    printf("static_initialized_var - \t0x%08x\n", &static_initialized_var);
-    printf("global_var - \t0x%08x\n", &global_var);
-    printf("static_var - \t0x%08x\n", &static_var);
-    printf("heap_var_ptr - \t0x%08x\n", heap_var_ptr);
-    printf("stack_var - \t0x%08x\n", stack_var);
+    printf("0x%08x: global_initialized_var\n", &global_initialized_var);
+    printf("0x%08x: static_initialized_var\n", &static_initialized_var);
+    printf("0x%08x: global_var\n", &global_var);
+    printf("0x%08x: static_var\n", &static_var);
+    printf("0x%08x: heap_var_ptr\n", heap_var_ptr);
+    printf("0x%08x: stack_var\n", stack_var);
+    printf("0x%08x: stack_var2\n", stack_var2);
     function();
-    
+
 }
