05af2933b985f6235b743f222ce01a04d5574d9d
Popkov Ilya <popkov.ilya.alexandrovich@gmail.com>
2022-01-03 00:44:18 +0300

    49:39

--- Python freecodecamp/code/app.py
+++ Python freecodecamp/code/app.py
@@ -1,5 +1,14 @@
+from math import *
+
+var = "Python"
+another_var = "eAsY"
+float_var = 50.105
+bool_var = True
+STRING_var = var[0]
 print("   /|")
-print("  / |")
+# print("  / |")
+phrase = var + " is " + another_var + " " + str(float_var) + " thousand times"
+print(phrase.lower().replace("0", "000"))
 print(" /  |")
 print("/___|")
-print("PyCharm is complicated")
+print(pow(2, 300))
